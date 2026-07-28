39757f73e6d158d4d203d1753a334ec6839ad146
Manasamadhugam <manasamadhugam@gmail.com>
2026-07-28 13:44:11 +0530

    Tokens

---
+++ day1.py
@@ -0,0 +1,23 @@
+print("hello codegnan")
+#dentifires
+#literals
+#operators
+#punctuators
+name = 'Manasa'
+print(name)
+email_id = 'manasamadhugam@gmail.com'
+print('email_id')
+Mokshith = 4
+print(Mokshith)
+true = 45
+mobileno = 7013154229
+print(7013154229)
+print("da23")
+length = 5 
+breadth = 3
+area = length * breadth
+print(area)
+name, location,ahe = 'Codeganan','Hyd',
+print(name)
+print(location)
+print(age)
