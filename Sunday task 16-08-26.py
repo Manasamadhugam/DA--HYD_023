10a9e7801d9d0fea1186fc21ff89dce2713987cb
Manasamadhugam <manasamadhugam@gmail.com>
2026-08-17 22:43:13 +0530

    list methods tuple usage

--- Sunday task 16-08-26.py
+++ Sunday task 16-08-26.py
@@ -1,4 +1,4 @@
-'''1
+'''
 #Students Marks manager -1
 marks = []
 for i in range(3):
@@ -13,7 +13,7 @@ removed_mark = marks.pop()
 print("Removed mark:", removed_mark)
 print("Final marks:", marks)
 print("Number of marks:", len(marks))
- '''   
+'''    
 '''
 #Number List Analyser -2
 numbers = [20,10,30,20,40,20]
@@ -28,7 +28,7 @@ if num in numbers:
     print("Index:", numbers.index(num))
 else:
     print("Number is not available")
- '''   
+'''  
 '''
 #Even and Odd Number Separator-3
 numbers = [10, 15, 20, 25, 30,35]
@@ -64,27 +64,39 @@ for name in names:
 
 '''
 # Course Student Comparison -5
-python_students = {"Asha", "Rahul", "John", "Meera"}
+python_students = {'Asha','Rahul','John','Meera'}
+da_students = {'Rahul','Meera','Arun'}
+print('both courses:',python_students | da_students)
+print('both courses students:',python_students & da_students)
+print('only python:', python_students - da_students)
+print('only one course:', python_students ^ da_students)
+print('DA subset of python:',da_students.issubset(python_students))
+print('python superset of DA:', python_students.issuperset(da_students))
+print('Disjoint:', python_students.isdisjoint(da_students))
+print('common students:')
+for student in python_students & da_students:
+ python_students = {"Asha", "Rahul", "John", "Meera"}
 da_students = {"Rahul", "Meera", "Arun"}
-
 print("Union:", python_students.union(da_students))
 print("Intersection:", python_students.intersection(da_students))
 print("Only Python:", python_students.difference(da_students))
 print("Only one course:", python_students.symmetric_difference(da_students))
-
-print("DA subset of Python:", da_students.issubset(python_students))
-print("Python superset of DA:", python_students.issuperset(da_students))
-print("Both are disjoint:", python_students.isdisjoint(da_students))
-
-print("Union students:")
-for i in python_students.union(da_students):
-    print(i)
-
-print("Common students:")
-for i in python_students.intersection(da_students):
-    print(i)
+if da_students.issubset(python_students):
+    print("DA is a subset of Python")
+else:
+    print("DA is not a subset of Python")
+if python_students.issuperset(da_students):
+    print("Python is a superset of DA")
+else:
+    print("Python is not a superset of DA")
+if python_students.isdisjoint(da_students):
+    print("Students are disjoint")
+else:
+    print("Students are not disjoint")
+print("Students learning both:")
+for student in python_students.intersection(da_students):
+    print(student)
 '''
-
 
 
 
